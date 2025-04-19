4b93cabc8a1ffc57af7cda7199a97461fe5e5720
Your Name <you@example.com>
2025-04-19 09:40:01 -0400

    new try

--- hashmap.c
+++ hashmap.c
@@ -40,8 +40,9 @@ int is_equal(void* key1, void* key2){
 
 void insertMap(HashMap * map , char * key, void * value) {
     long pos = hash(key, map->capacity);
-    
+
     while (map->buckets[pos] != NULL && map->buckets[pos]->key != NULL) {
+        if (map->size / map->capacity >= 0.7) enlarge(map);
         if (is_equal(map->buckets[pos]->key, key)) return; 
         pos = (pos + 1) % map->capacity;
         map->current = pos;
