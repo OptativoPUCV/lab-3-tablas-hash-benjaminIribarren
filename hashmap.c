fd0d133e61f45dd48a1dcfd7e88dfe392312e0ad
Your Name <you@example.com>
2025-04-19 08:47:57 -0400

    new try

--- hashmap.c
+++ hashmap.c
@@ -56,7 +56,7 @@ void enlarge(HashMap * map) {
     enlarge_called = 1; //no borrar (testing purposes)
     size_t new_capacity = map->capacity * 2;
     Pair ** new_buckets = NULL;
-    Pair ** new_buckets = (Pair **)malloc(new_capacity * sizeof(Pair *));
+    new_buckets = (Pair **)malloc(new_capacity * sizeof(Pair *));
     for (size_t i = 0; i < new_capacity; i++) {
         new_buckets[i] = NULL;
     }
